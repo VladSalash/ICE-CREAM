5881e9b2220ea0b1a94ce1e8c8970c4278c475cd
VladSalash <salashchenko99@gmail.com>
2022-12-10 00:51:29 +0300

    update

---
+++ src/js/modal-about.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-about]'),
+    openModalBtn2: document.querySelector('[data-modal-open-about2]'),
+    closeModalBtn: document.querySelector('[data-modal-close-about]'),
+    modal: document.querySelector('[data-modal-about]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
